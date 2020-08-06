adcfb82a6d0020a5b2467559275e3576f5894f1d
emidioandre <61055956+emidioandre@users.noreply.github.com>
2020-08-06 16:08:55 -0300

    Update
    Update

--- mypubsub/settings.py
+++ mypubsub/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'vq9#+%6z%_lauv7q2tfu5lb%pxlat&rdmmt1y5886ed#ob00!f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
